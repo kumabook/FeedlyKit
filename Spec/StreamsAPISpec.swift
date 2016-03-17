1e8e9beaaa9532fccfc87e36351a212c232b4488
Hiroki Kumamoto <kumabook@live.jp>
2016-03-17 21:43:57 +0900

    Refactoring StreamsAPISpec

--- Spec/StreamsAPISpec.swift
+++ Spec/StreamsAPISpec.swift
@@ -15,42 +15,44 @@ import Nimble
 class StreamsAPISpec: QuickSpec {
     let perPage = 5
     let feedId  = "feed/http://kumabook.github.io/feed.xml"
+
     let client: CloudAPIClient = CloudAPIClient(target: SpecHelper.target)
     var entryIds:   [String] = []
-    var statusCode: Int      = 0
     var entries:    [Entry]  = []
 
     override func spec() {
         describe("fetchIds") {
+            var statusCode = 0
             beforeEach {
                 let params = PaginationParams()
                 params.count = self.perPage
                 self.client.fetchEntryIds(self.feedId, paginationParams: params) {
                     guard let code = $0.response?.statusCode,
                           let val  = $0.result.value else { return }
-                    self.statusCode = code
-                    self.entryIds   = val.ids
+                    statusCode    = code
+                    self.entryIds = val.ids
                 }
             }
             it ("fetch entries ids") {
-                expect(self.statusCode).toFinally(equal(200))
+                expect(statusCode).toFinally(equal(200))
                 expect(self.entryIds.count).toFinally(equal(self.perPage))
             }
         }
 
         describe("fetchContents") {
+            var statusCode = 0
             beforeEach {
                 let params = PaginationParams()
                 params.count = self.perPage
                 self.client.fetchContents(self.feedId, paginationParams: params) {
                     guard let code = $0.response?.statusCode,
                           let val  = $0.result.value else { return }
-                    self.statusCode = code
-                    self.entries    = val.items
+                    statusCode   = code
+                    self.entries = val.items
                 }
             }
             it ("fetches entries contents") {
-                expect(self.statusCode).toFinally(equal(200))
+                expect(statusCode).toFinally(equal(200))
                 expect(self.entries.count).toFinally(equal(self.perPage))
             }
         }
@@ -59,16 +61,18 @@ class StreamsAPISpec: QuickSpec {
             let params = PaginationParams()
             params.count = self.perPage
             params.continuation = ""
+            var statusCode = 0
             func clear() {
                 self.entryIds       = []
                 params.continuation = ""
             }
             func fetchIds(params: PaginationParams, times: Int) {
                 if times == 0 { return }
+                sleep(1);
                 self.client.fetchEntryIds(self.feedId, paginationParams: params) {
                     guard let code = $0.response?.statusCode,
                           let val  = $0.result.value else { return }
-                    self.statusCode     = code
+                    statusCode     = code
                     self.entryIds.appendContentsOf(val.ids)
                     params.continuation = val.continuation
                     if times > 0 && params.continuation != nil {
@@ -77,34 +81,37 @@ class StreamsAPISpec: QuickSpec {
                 }
             }
             context("first page") {
+                statusCode = 0
                 beforeEach {
                     clear()
                     fetchIds(params, times: 1)
                 }
-                it ("fetch return first page") {
-                    expect(self.statusCode).toFinally(equal(200))
+                it ("fetches first page") {
+                    expect(statusCode).toFinally(equal(200))
                     expect(self.entryIds.count).toFinally(equal(self.perPage))
                 }
             }
             context("second page") {
+                statusCode = 0
                 beforeEach {
                     clear()
                     fetchIds(params, times: 2)
                 }
-                it ("fetch return first page") {
-                    expect(self.statusCode).toFinally(equal(200))
+                it ("fetches second page") {
+                    expect(statusCode).toFinally(equal(200))
                     expect(self.entryIds.count).toFinally(equal(self.perPage * 2))
                 }
             }
-            context("last page") {
+/*            context("last page") {
+                statusCode = 0
                 beforeEach {
                     clear()
                     fetchIds(params, times: Int.max)
                 }
-                it ("fetch return first page") {
+                it ("eventually fetches last page") {
                     expect(params.continuation).toEventually(beNil())
                 }
-            }
+            }*/
         }
     }
 }
